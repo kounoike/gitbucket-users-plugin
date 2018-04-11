1e2ac0f62f8830fb6f54690370b33204a87ca942
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2018-04-11 22:34:10 +0900

    bump version to 1.0.2

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "gitbucket-users-plugin"
 organization := "io.github.gitbucket"
-version := "1.0.1"
+version := "1.0.2"
 
 scalaVersion := "2.12.4"
 gitbucketVersion := "4.23.1"
--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -7,7 +7,11 @@ class Plugin extends gitbucket.core.plugin.Plugin {
   override val pluginId: String = "users"
   override val pluginName: String = "Users Plugin"
   override val description: String = "List users and groups of GitBucket plug-in"
-  override val versions: List[Version] = List(new Version("1.0.0"))
+  override val versions: List[Version] = List(
+    new Version("1.0.0"),
+    new Version("1.0.1"),
+    new Version("1.0.2")
+  )
 
   override val globalMenus = Seq(
     (context: Context) => Some(Link("users", "Users", "users"))
